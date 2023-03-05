bad9abb012973f13a7889d14027da5a7f5124773
Juansecode <juan.sebastianri28@gmail.com>
2023-03-04 19:53:12 -0500

    Instanciacion y ejecucion de los metodos de un objeto tipo Stack

--- Pilas/pila.py
+++ Pilas/pila.py
@@ -34,3 +34,30 @@ class Stack:
             print(current.data, end='->')
             current = current.next
         print()
+  
+stack = Stack()       
+        
+# Apilamos los datos dentro de la pila
+stack.push("Primer Elemento")
+stack.push("Segundo Elemento")
+stack.push("Tercero Elemento")
+stack.push("Cuarto Elemento")
+
+# Mostramos el dato que esta en la cabeza 
+print("Primer dato en salir:")
+print(stack.peek())
+print("-"*50)
+
+# Mostramos todos los datos que hay dentro de la pila 
+print("Elementos de la pila: ")
+stack.display()
+print("-"*50)
+
+# Desapilamos dos elementos 
+stack.pop()
+stack.pop()
+
+# Mostramos la pila ahora con los elementos desapilados
+print("Elementos de la pila luedo de desapilarla dos veces: ")
+stack.display()
+print("-"*50)
