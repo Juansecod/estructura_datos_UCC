4bda59533bc3c3b373def00355fc76e74d602fc1
Juansecode <juan.sebastianri28@gmail.com>
2023-03-13 11:53:49 -0500

    Add to a queue the response of requests

--- Colas/app/client/main.py
+++ Colas/app/client/main.py
@@ -1,15 +1,63 @@
-import pandas as pd
+from pandas import DataFrame
 import requests
 import threading
+from os import system
 
-def consulta(url):
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+    
+    def is_empty(self):
+        return self.first is None
+    
+    def append(self, value):
+        new_node = Node(value)
+        if self.is_empty():
+            self.first = new_node
+        else:
+            self.last.next = new_node
+        self.last = new_node
+    
+    def pop(self):
+        if self.is_empty():
+            return None
+        value = self.first.value
+        self.first = self.first.next
+        if self.first is None:
+            self.last = None
+        return value
+    
+    def display(self):
+        if self.is_empty():
+            print("The Queue is empty...")
+        current = self.first
+        while current:
+            print(current.value, end=f"\n\n")
+            current = current.next
+        print("-"*100)    
+
+urls = Queue()
+responses = Queue()
+
+def fetch(url):
     r = requests.get(url)
-    print(r.json())
+    df = DataFrame(r.json())
+    responses.append(df)
+    system("cls")
+    urls.display()
+    responses.display()
 
-hilos = []
+threads = []
 for url in ["http://localhost:8000/productos", "http://localhost:8000/carts", "http://localhost:8000/users", ]:
-    hilo = threading.Thread(target=consulta, args=[url])
-    hilos.append(hilo)
+    urls.append(url)
+    thread = threading.Thread(target=fetch, args=[url])
+    threads.append(thread)
 
-for f in hilos:
+for f in threads:
     f.start()
