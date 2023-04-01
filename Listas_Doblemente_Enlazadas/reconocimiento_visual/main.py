c31ffbbfc3568b7252d4474846a604a0de021d66
Juan Sebastian <juan.sebastianri28@gmail.com>
2023-04-01 15:18:04 +0000

    Creacion del modelo para el reconocimiento de flores

--- Listas_Doblemente_Enlazadas/reconocimiento_visual/main.py
+++ Listas_Doblemente_Enlazadas/reconocimiento_visual/main.py
@@ -10,31 +10,22 @@ from keras.models import Sequential
 
 import pathlib
 
-class DataSet():
+""" class DataSet():
     def __init__(self, dataset_url, file_name) -> None:
         data_dir = tf.keras.utils.get_file(file_name, origin=dataset_url, untar=True)
         self.data_dir = pathlib.Path(data_dir)
+        print(self.data_dir)
 
     def get_size(self):
         image_count = len(list(self.data_dir.glob('*/*.jpg')))
-        print(image_count)
+        print(image_count) """
 
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
+print(data_dir)
 
-flowers = DataSet(dataset_url,"flowers_photos")
-
-flowers.get_size()
-
-
-
-
-
-
-
-
-
-
-""" batch_size = 32
+batch_size = 32
 img_height = 180
 img_width = 180
 
@@ -55,4 +46,67 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   batch_size=batch_size)
 
 class_names = train_ds.class_names
-print(class_names) """
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+data_augmentation = keras.Sequential(
+  [
+    layers.RandomFlip("horizontal",
+                      input_shape=(img_height,
+                                  img_width,
+                                  3)),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+  ]
+)
+
+num_classes = len(class_names)
+
+model = Sequential([
+  data_augmentation,
+  layers.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes, name="outputs")
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs = 15
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+""" Codigo que testea la inteligencia artificial """
+
+sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
+
+img = tf.keras.utils.load_img(
+    sunflower_path, target_size=(img_height, img_width)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score)))
