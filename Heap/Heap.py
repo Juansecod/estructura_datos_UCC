474eba64dea07cd354257283745b9f8f639396fb
Juansecode <juan.sebastianri28@gmail.com>
2023-05-05 16:26:44 -0500

    Implementacion del metodo constructor y del metodo insertar

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -1,35 +1,44 @@
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
 class Heap():
-    def __init__(self):
-        self.root = None
-
-    def heapify(self, array):
-        for i in array:
-            self.__recursive_insert(i)
-        print(self.root.data)
-
-"""     def __recursive_insert(self, data, node):
-        if self.root is None:
-            self.root = Node(data)
-        else: """
-            
-
-            
+    def __init__(self, arr = []):
+        self.arr = []
+        self.size = 0
+        for i in arr:
+            self.insert(i)
         
-
-
-
+    def __parent(self, i):
+        return (i - 1) // 2
     
-
-
+    def __heapify(self, i):
+        if self.size == 0:
+            return
+        
+        temp = self.arr[i]
+        parent = self.__parent(i)
+        data_parent = self.arr[parent]
+        
+        if temp > data_parent:
+            self.arr[i] = data_parent
+            self.arr[parent] = temp
+        if parent > 0:
+            self.__heapify(parent)    
+            
+    def insert(self, data):
+        self.arr.append(data)
+        self.size += 1
+        self.__heapify(self.size - 1)
 
 
 arr = [30, 12, 8, 90, 37, 50 ]
 
 heap = Heap()
-heap.heapify(arr)
+print(heap.arr)
+
+for i in arr:
+    heap.insert(i)
+    
+print("Heap sin usar el constructor:", end=" ")
+print(heap.arr)
+
+heap_init = Heap(arr)
+print("Heap usando el constructor:", end=" ")
+print(heap_init.arr)
--- Heap/README.md
+++ Heap/README.md
@@ -1,4 +1,5 @@
 # Heap
+
 Heap se encarga de distribuir todos los datos en un arbol binario. El metodo de llenado del arbol es distinto a como se trabaja normalmente. Para el llenado se nombra a cada uno de los nodos con un indice.
 
 Es muy util para encotrar rutas, y es el mejor algoritmo para el ordenamiento.
@@ -6,7 +7,7 @@ Es muy util para encotrar rutas, y es el mejor algoritmo para el ordenamiento.
 Heap superior: Se ordena los elementos de arriba hacia abajo. El elemento mayor va estar en la primera posicion, osea el indice 0. Se van comparando los datos que ingresan y se intercambian recursivamente los numeros mayores. Para eliminar buscamos toda la referencia y seleccionamos el inferior para intercambiarlo con el primer nodo, y luego recursivamente organizar los nodos
 
 Ejemplo:
-Input -> 30, 12, 8, 90, 37, 50 
+Input -> 30, 12, 8, 90, 37, 50
 
 Output ->               90
                     /       \
